915b2daaefb4131b31df39878a691637da83065a
Tandoh3 <tandohn2@gmail.com>
2023-07-28 15:22:17 +0000

    String Operators

--- FirstQuery.sql
+++ FirstQuery.sql
@@ -375,5 +375,55 @@ GROUP BY JobTitle
 SELECT * 
 FROM #Temp_Employee2
 
-
 --Stored Procedures 
+
+/*
+String Functions - TRIM, LTRIM, RTRIM, REPLACE, SUBSTRING, UPPER, LOWER
+*/
+
+CREATE TABLE EmployeeErrors (
+EmployeeID varchar(50), 
+FirstName varchar(50), 
+LastName varchar(50)
+)
+
+INSERT INTO EmployeeErrors VALUES 
+('1001 ', 'Jimbo', 'Halbert'),
+(' 1002', 'Pamela', 'Beasely'), 
+('1005', 'TOby', 'Flenderson - Fired')
+
+SELECT * 
+FROM EmployeeErrors
+
+
+--Using TRIM, LTRIM, RTRIM 
+SELECT EmployeeID, TRIM(EmployeeID) AS IDTRIM 
+FROM EmployeeErrors
+
+SELECT EmployeeID, LTRIM(EmployeeID) AS IDTRIM 
+FROM EmployeeErrors
+
+SELECT EmployeeID, RTRIM(EmployeeID) AS IDTRIM 
+FROM EmployeeErrors
+
+
+-- Using REPLACE 
+SELECT LastName, REPLACE(LastName, '- Fired', ' ') AS LastNameFixed
+FROM EmployeeErrors
+
+
+-- Using SUBSTRING 
+
+
+SELECT SUBSTRING(FirstName, 1, 3)
+FROM EmployeeErrors
+
+
+
+--Using UPPER and LOWER 
+SELECT FirstName, LOWER(FirstName)
+FROM EmployeeErrors
+
+
+SELECT FirstName, UPPER(FirstName)
+FROM EmployeeErrors
